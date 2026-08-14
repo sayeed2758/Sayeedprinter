1eb13ba41b2662c494f9715dbd6f069a6494e3c5
sayeed2758 <sayeedbackupmail@gmail.com>
2026-08-14 06:37:50 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
+const browseBtn = document.getElementById("browseBtn");
+const fileInput = document.getElementById("fileInput");
 
+browseBtn.addEventListener("click", () => {
+    fileInput.click();
+});
+
+fileInput.addEventListener("change", () => {
+    const file = fileInput.files[0];
+
+    if (!file) return;
+
+    console.log("Selected file:", file.name);
+});
