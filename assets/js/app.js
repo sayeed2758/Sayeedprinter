73931ae6d4c43280babc5d37df8d5909e80f8f63
sayeed2758 <sayeedbackupmail@gmail.com>
2026-08-14 07:16:57 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,15 @@ const processing = document.getElementById("processing");
 const previewArea = document.getElementById("previewArea");
 
 fileInput.addEventListener("change", function () {
+previewArea.innerHTML = "";
 
+if (file.type.startsWith("image/")) {
+    const image = document.createElement("img");
+
+    image.src = URL.createObjectURL(file);
+
+    previewArea.appendChild(image);
+}
     const file = this.files[0];
 
     if (!file) return;
